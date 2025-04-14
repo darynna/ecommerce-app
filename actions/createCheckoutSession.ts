1d9f7ce37ef2e2cf32203bc57e5830932ff67b5e
Daryna Holikova <dgolikova0@gmail.com>
2025-04-14 14:15:38 +0100

    changes

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -63,6 +63,7 @@ export async function createCheckoutSession(
                 quantity: item.quantity
             }))
         })
+        return session.url
     } catch (error) {
         console.log("Error creating cheackout session ", error)
     }
