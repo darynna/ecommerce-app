4f25f7986a563a806c5d1aa9ad48d703e2128b6d
Daryna Holikova <dgolikova0@gmail.com>
2025-05-13 18:34:05 +0100

    implementing Sanity’s Real Time Visual Editor

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,7 +6,8 @@
 
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { structureTool } from 'sanity/structure'
+import { presentationTool } from 'sanity/presentation'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
@@ -23,6 +24,14 @@ export default defineConfig({
     structureTool({structure}),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    visionTool({ defaultApiVersion: apiVersion }),
+    presentationTool({
+      previewUrl: {
+        preview: "/",
+        previewMode: {
+          enable: "/draft-mode/enable"
+        }
+      }
+    })
   ],
 })
